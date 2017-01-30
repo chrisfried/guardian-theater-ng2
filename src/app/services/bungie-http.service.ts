4347e895fdfcb3b7487507c207f063f0e5783aeb
Chris Fried <fried.chris@gmail.com>
2017-01-30 14:21:52 -0600

    Key switch.

--- src/app/services/bungie-http.service.ts
+++ src/app/services/bungie-http.service.ts
@@ -1,13 +1,32 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class BungieHttpService {
+  private _origin: string;
+  private _apiKey: string;
 
-  constructor(private http: Http) {}
+  constructor(
+    private http: Http
+  ) {
+    this._origin = window.location.protocol + '//' + window.location.hostname;
+    switch (this._origin) {
+      case 'http://dev.guardian.theater':
+        this._apiKey = '4da0bc9d76774c5696ea2703b129a2cd';
+        break;
+
+      case 'https://beta.guardian.theater':
+        this._apiKey = '97c176d9f0f144f0b70bb69a455234a6';
+        break;
+
+      case 'https://guardian.theater':
+        this._apiKey = 'fc91f657672b41189d2682be8eb51697';
+        break;
+    }
+  }
 
   createAuthorizationHeader(headers: Headers) {
-    headers.append('x-api-key', '4da0bc9d76774c5696ea2703b129a2cd');
+    headers.append('x-api-key', this._apiKey);
   }
 
   get(url) {
