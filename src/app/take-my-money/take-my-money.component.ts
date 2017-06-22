b192a62d091623a3ad6dca88a1a513ce9a31235a
Chris Fried <fried.chris@gmail.com>
2017-06-22 11:04:25 -0700

    Basic donation page.

---
+++ src/app/take-my-money/take-my-money.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-take-my-money',
+  templateUrl: './take-my-money.component.html',
+  styleUrls: ['./take-my-money.component.scss']
+})
+export class TakeMyMoneyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
