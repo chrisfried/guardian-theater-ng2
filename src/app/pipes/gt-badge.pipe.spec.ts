06cb231c77b6c4cf9f5be15c66a75e5346bbf61e
Chris Fried <fried.chris@gmail.com>
2017-07-18 00:24:46 -0500

    Removing unused modules becuase --prod doesn't like them.

--- src/app/pipes/gt-badge.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GtBadgePipe } from './gt-badge.pipe';
-
-describe('GtBadgePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GtBadgePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
