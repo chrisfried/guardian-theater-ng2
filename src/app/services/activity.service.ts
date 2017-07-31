2ca6f3ad8187c497d3f650099e7c9e5835c03b9b
Dustin Rue <dustin.rue@10up.com>
2017-07-30 22:04:08 -0500

    use beta endpoint

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -434,7 +434,7 @@ export class ActivityService implements OnDestroy {
                       response: {}
                     }) => {
                       if (!gamer.checked && gamer.gamertag) {
-                        return 'http://dustinrue.com:3000/gameclips/gamertag/' + gamertag + '/titleid/247546985';
+                        return 'https://api.xboxrecord.us/gameclips/gamertag/' + gamertag + '/titleid/247546985';
                       } else {
                         return '';
                       }
