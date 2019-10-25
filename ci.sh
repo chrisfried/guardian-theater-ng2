c5a2d9c89ba11133460d4cf0630cbc5d93ac17d2
Dustin Rue <dustin.rue@10up.com>
2019-10-24 21:10:48 -0500

    more building

--- ci.sh
+++ ci.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pushd docker/ssl
+cd docker/ssl
 
 if [ ! -f dev.key ]; then
   openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout dev.key -days 3560 -out dev.crt -config certificate.cnf
 fi
 
-popd
+cd -
 
 docker build --no-cache . -t gt
 docker run -d --name gt -p 3000:443 gt
